3f7c71b2151e9abb4a1dba2f457ca52e2a5f4271
Otávio Cardoso <ana.wrab@gmail.com>
2022-04-13 09:41:57 -0300

    contato
    contato basico

--- contato.php
+++ contato.php
@@ -1,15 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title> Styl</title>
+</head>
+<body>
+    
 
-include_once 'includes/_header.php'
-include_once 'includes/_footer.php'
-
-?>
 
 <div>
   <h1> Contato </h1>  
   <h3> Nos contate pelo </h3>
   <li> Insta: @styl </li>
   <li> Email: styl@contato.com </li>
+  <li>  </li>
 
 
 </div>
+
+</body>
+</html>
