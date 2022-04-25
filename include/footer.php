b1925ae35c0b115f440e25f53baaa343d46f6d47
Biancaa <103117366+BiancaFiRodrigues@users.noreply.github.com>
2022-04-25 09:23:18 -0300

    footer

---
+++ include/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <a class="navbar-brand" href="../styl/index.php">styl</a>
+    <span style="font-size: 1rem; color: white;">Também atendemos pelo instagram @styl</span>
+</footer>
